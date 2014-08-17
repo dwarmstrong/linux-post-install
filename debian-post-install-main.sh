adc993e699d4116faf9e7c7753f39949d14f93cf
Daniel Wayne Armstrong <daniel@circuidipity.com>
2014-08-17 12:42:11 -0400

    add color codes

--- debian-post-install-main.sh
+++ debian-post-install-main.sh
@@ -36,6 +36,36 @@ deb_branch=""
 deb_package_list=""
 
 # Functions
+echo_red() {
+echo -e "\E[1;31m$1"
+echo -e '\e[0m'
+}
+
+echo_green() {
+echo -e "\E[1;32m$1"
+echo -e '\e[0m'
+}
+
+echo_yellow() {
+echo -e "\E[1;33m$1"
+echo -e '\e[0m'
+}
+
+echo_blue() {
+echo -e "\E[1;34m$1"
+echo -e '\e[0m'
+}
+
+echo_magenta() {
+echo -e "\E[1;35m$1"
+echo -e '\e[0m'
+}
+
+echo_cyan() {
+echo -e "\E[1;36m$1"
+echo -e '\e[0m'
+}
+
 penguinista() {
 cat << _EOF_
 
