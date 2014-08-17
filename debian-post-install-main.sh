afe3bda603d212468a83ed815e9ee03789ec27b8
Daniel Wayne Armstrong <daniel@circuidipity.com>
2014-08-16 22:38:33 -0400

    first commit

---
+++ debian-post-install-main.sh
@@ -0,0 +1,618 @@
+#!/bin/bash
+set -eu
+
+# Copyright (c) 2014 Daniel Wayne Armstrong. All rights reserved.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License (GPLv2) published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the LICENSE file for more details.
+
+# Script variables
+script_name="debian-post-install-main.sh"
+script_description="stable|testing|unstable branch configuration"
+script_synopsis="$script_name [OPTION] [PACKAGE_LIST]"
+script_git="https://github.com/vonbrownie"
+script_src="$script_git/linux-post-install/blob/master/$script_name"
+
+# Script global directory variables
+tmp_dir="/tmp/tmp.debian-post-install"
+current_dir="."
+profile_dir="${current_dir}/profiles/debian/"
+log_file="${current_dir}/debian-post-install-log.txt"
+
+# Debian variables
+deb_stable="wheezy"
+deb_testing="jessie"
+deb_unstable="sid"
+apt_preferences="/etc/apt/preferences"
+apt_sources_list="/etc/apt/sources.list"
+deb_archive="http://http.debian.net/debian/"
+deb_arch=$(dpkg --print-architecture)
+dpkg_info="/var/lib/dpkg/info"
+deb_branch=""
+deb_package_list=""
+
+# Functions
+penguinista() {
+cat << _EOF_
+
+(O<
+(/)_
+_EOF_
+}
+
+available_options() {
+cat << _EOF_
+OPTIONS
+  -h    print command syntax and options
+  -i    import a Debian package list for installation
+EXAMPLE
+  Install packages from 'package-list.txt':
+  (as_root)# ./$script_name -i package-list.txt
+
+_EOF_
+}
+
+long_description() {
+clear
+cat << _EOF_
+$( penguinista ) .: $script_name -- $script_description :.
+
+SYNOPSIS
+  $script_synopsis
+SOURCE
+  $script_src
+
+Howdy! Ideally this script is run following a fresh installation of
+Debian GNU/Linux.
+
+See: "Debian Wheezy Minimal Install"
+http://www.circuidipity.com/install-debian-wheezy-screenshot-tour.html
+
+"Install Debian using grml-debootstrap"
+http://www.circuidipity.com/grml-debootstrap.html
+
+This system will be configured to track a choice of Debian's ${deb_stable}/stable,
+${deb_testing}/testing, or ${deb_unstable}/unstable branch with the option of installing the
+Openbox window manager + extra apps suitable for a desktop environment.
+
+## TIP ##
+Import a list of packages that duplicate the configuration from another system
+running Debian.
+
+See: "Duplicate Debian package selection on multiple machines"
+http://www.circuidipity.com/dpkg-duplicate.html
+
+... and run this script with option '-i' and the location of the package list.
+
+_EOF_
+available_options
+}
+
+run_options() {
+while getopts ":hi:" OPT
+do
+    case $OPT in
+        h)
+            long_description
+            exit 0
+            ;;
+        i)
+            deb_package_list="$OPTARG"
+            break
+            ;;
+        :)
+            printf "\n$( penguinista ) .: Option '-$OPTARG' missing argument.\n"
+            available_options
+            exit 1
+            ;;
+        *)
+            printf "\n$( penguinista ) .: Invalid option '-$OPTARG'\n"
+            available_options
+            exit 1
+            ;;
+    esac
+done
+}
+
+invalid_reply() {
+printf "\n'$REPLY' is invalid input...\n"
+}
+
+invalid_reply_yn() {
+printf "\n'$REPLY' is invalid input. Please select 'Y(es)' or 'N(o)'...\n"
+}
+
+confirm_start() {
+while :
+do
+    read -n 1 -p "Run script now? [yN] > "
+    if [[ $REPLY == [yY] ]]
+    then
+        printf "\nLet's roll then ...\n"
+        sleep 2
+        break
+    elif [[ $REPLY == [nN] || $REPLY == "" ]]
+    then
+        penguinista
+        exit
+    else
+        invalid_reply_yn
+    fi
+done
+}
+
+test_root() {
+if [[ $UID -ne 0 ]]
+then
+    printf "\n$( penguinista ) .: $script_name requires ROOT privileges to do its job.\n"
+    exit 1
+else
+    if [[ ! -d "$tmp_dir" ]]
+    then
+        mkdir $tmp_dir
+    fi
+fi
+}
+
+interface_found() {
+ip link | awk '/mtu/ {gsub(":",""); printf "\t%s", $2} END {printf "\n"}'
+}
+
+test_connect() {
+if ! $(ip addr show | grep "state UP" &>/dev/null)
+then
+    printf "\n$( penguinista ) .: $script_name requires an active network interface."
+    printf "\nINTERFACES FOUND\n"
+    interface_found
+    exit 1
+fi
+}
+
+test_package_list() {
+if [[ ! -z $deb_package_list && ! -e $deb_package_list ]]
+then
+    printf "\n$( penguinista ) .: '$deb_package_list' not found.\n"
+    exit 1
+fi
+}
+
+test_conditions() {
+test_root
+test_connect
+test_package_list
+}
+
+apt_preferences_stable() {
+if [[ -e $apt_preferences ]]
+then
+    cp $apt_preferences ${apt_preferences}.$(date +%Y%m%dT%H%M%S).bak
+fi
+cat > $apt_preferences << _EOF_
+# Configure default preferences in package installation
+# * unlisted repositories are auto-ranked 500
+# * installed packages are ranked 100
+
+Package: *
+Pin: release n=$deb_stable
+Pin-Priority: 900
+_EOF_
+}
+
+apt_preferences_testing() {
+if [[ -e $apt_preferences ]]
+then
+    cp $apt_preferences ${apt_preferences}.$(date +%Y%m%dT%H%M%S).bak
+fi
+cat > $apt_preferences << _EOF_
+# Configure default preferences in package installation
+# * unlisted repositories are auto-ranked 500
+# * installed packages are ranked 100
+
+Package: *
+Pin: release n=$deb_testing
+Pin-Priority: 900
+_EOF_
+}
+
+apt_preferences_unstable() {
+if [[ -e $apt_preferences ]]
+then
+    cp $apt_preferences ${apt_preferences}.$(date +%Y%m%dT%H%M%S).bak
+fi
+cat > $apt_preferences << _EOF_
+# Configure default preferences in package installation
+# * unlisted repositories are auto-ranked 500
+# * installed packages are ranked 100
+
+Package: *
+Pin: release a=unstable
+Pin-Priority: 900
+
+Package: *
+Pin: release a=experimental
+Pin-Priority: 1
+_EOF_
+}
+
+apt_sources_stable() {
+if [[ -e $apt_sources_list ]]
+then
+    cp $apt_sources_list ${apt_sources_list}.$(date +%Y%m%dT%H%M%S).bak
+fi
+clear
+cat > $apt_sources_list << _EOF_
+### $deb_stable ###
+deb $deb_archive $deb_stable main contrib non-free
+deb-src $deb_archive $deb_stable main contrib non-free
+
+### $deb_stable security ###
+deb http://security.debian.org/ $deb_stable/updates main contrib non-free
+#deb-src http://security.debian.org/ $deb_stable/updates main contrib non-free
+
+### ${deb_stable}-updates ###
+deb $deb_archive ${deb_stable}-updates main non-free contrib
+#deb-src deb $deb_archive ${deb_stable}-updates main non-free contrib 
+
+### backports ###
+deb $deb_archive ${deb_stable}-backports main contrib non-free
+deb http://mozilla.debian.net/ ${deb_stable}-backports iceweasel-release
+
+### multimedia ###
+deb http://www.deb-multimedia.org $deb_stable main non-free
+
+_EOF_
+apt-get update
+}
+
+apt_sources_testing() {
+if [[ -e $apt_sources_list ]]
+then
+    cp $apt_sources_list ${apt_sources_list}.$(date +%Y%m%dT%H%M%S).bak
+fi
+clear
+cat > $apt_sources_list << _EOF_
+### $deb_testing ###
+deb $deb_archive $deb_testing main contrib non-free
+deb-src $deb_archive $deb_testing main contrib non-free
+
+### $deb_testing security ###
+deb http://security.debian.org/ ${deb_testing}/updates main contrib non-free
+#deb-src http://security.debian.org/ ${deb_testing}/updates main contrib non-free
+
+### multimedia ###
+deb http://www.deb-multimedia.org $deb_testing main non-free
+
+_EOF_
+apt-get update
+}
+
+apt_sources_unstable() {
+if [[ -e $apt_sources_list ]]
+then
+    cp $apt_sources_list ${apt_sources_list}.$(date +%Y%m%dT%H%M%S).bak
+fi
+clear
+cat > $apt_sources_list << _EOF_
+### unstable ###
+deb $deb_archive unstable main contrib non-free
+deb-src $deb_archive unstable main contrib non-free
+
+### experimental ###
+deb $deb_archive experimental main
+
+### multimedia ###
+deb http://www.deb-multimedia.org unstable main non-free
+
+_EOF_
+apt-get update
+}
+
+apt_keys() {
+clear
+apt-get install -y debian-archive-keyring
+apt-get install deb-multimedia-keyring
+apt-get install pkg-mozilla-archive-keyring
+apt-get update
+apt-get -y dist-upgrade
+}
+
+apt_package_list() {
+local deb_packages
+deb_packages=$(mktemp)
+clear
+if [[ ! -z $deb_package_list && -e $deb_package_list ]]
+then
+    apt-cache dumpavail > "$deb_packages"
+    dpkg --merge-avail "$deb_packages"
+    rm -f "$deb_packages"
+    dpkg --clear-selections
+    dpkg --set-selections < $deb_package_list
+    apt-get dselect-upgrade
+fi
+}
+
+apt_package_purge() {
+local deb_package_purge
+deb_package_purge="gdm3 gnome-system-tools nautilus* libnautilus* \
+notification-daemon tumbler* libtumbler*"
+clear
+apt-get --purge remove $deb_package_purge
+}
+
+package_console() {
+local console_pkgs
+console_pkgs="build-essential dkms module-assistant colordiff \
+cryptsetup htop iproute iw lxsplit par2 pmount p7zip-full unrar \
+unzip rsync sudo sl tmux vim whois xz-utils wpasupplicant"
+clear
+apt-get install -y $console_pkgs
+}
+
+package_xorg() {
+local xorg_pkgs
+xorg_pkgs="xorg x11-utils xbacklight xdotool xfonts-terminus xterm rxvt-unicode"
+clear
+apt-get install -y $xorg_pkgs
+}
+
+package_openbox() {
+local openbox_pkgs
+openbox_pkgs="openbox obconf eject feh flashplugin-nonfree gksu gsimplecal \
+iceweasel icedtea-7-plugin openjdk-7-jre leafpad lxappearance \
+lxappearance-obconf menu mirage network-manager-gnome pavucontrol \
+qt4-qtconfig scrot suckless-tools tint2 thunar-volman vlc xarchiver \
+xfce4-notifyd xfce4-power-manager xfce4-settings xfce4-volumed \
+xscreensaver zenity"
+clear
+apt-get install -y $openbox_pkgs
+}
+
+package_theme() {
+local ubuntu_ver
+local cb_archive
+local cb_icons
+local cb_icons_deb
+ubuntu_ver="trusty"
+cb_archive="http://packages.crunchbang.org/waldorf/pool/main"
+cb_icons="faenza-crunchbang-icon-theme"
+cb_icons_deb="${cb_icons}_1.2-crunchang1_all.deb"
+
+# Theme - Numix
+# -------------
+# * http://numixproject.org/
+# * includes GTK2 + GTK3.6+ + Openbox + Xfce support
+apt-get install -y gtk2-engines gtk2-engines-murrine libgnomeui-0
+echo "### numix theme ###" >> $apt_sources_list
+echo "deb http://ppa.launchpad.net/numix/ppa/ubuntu $ubuntu_ver main" >> $apt_sources_list 
+echo "deb-src http://ppa.launchpad.net/numix/ppa/ubuntu $ubuntu_ver main" >> $apt_sources_list 
+apt-get update
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0F164EEB
+apt-get update
+apt-get install -y numix-gtk-theme
+
+# Icons - Faenza-Dark-Crunchbang
+# ------------------------------
+if [[ ! -e "${dpkg_info}/${cb_icons}.list" ]]
+then
+    clear
+    wget -P $tmp_dir ${cb_archive}/${cb_icons_deb}
+    dpkg -i ${tmp_dir}/$cb_icons_deb
+fi
+
+# Fonts - Droid Sans
+# ------------------
+apt-get install -y fonts-liberation fonts-droid
+
+# Theme Config Utils
+# ------------------
+# * Openbox: obconf
+# * GTK+LXDE: lxappearance
+# * Xfce4: xfce4-settings-manager
+# * QT: qtconfig-qt4
+# * Xfce4: xfce4-settings
+apt-get install -y obconf lxappearance lxappearance-obconf \
+qt4-qtconfig xfce4-settings
+}
+
+package_backport() {
+local backport_pkgs
+backport_pkgs="iceweasel"
+if [[ $deb_branch == $deb_stable ]]
+then
+    apt-get install -y -t ${deb_stable}-backports $backport_pkgs
+fi
+}
+
+package_extra() {
+local cb_archive
+local cb_pnmixer
+local cb_pnmixer_deb
+cb_archive="http://packages.crunchbang.org/waldorf/pool/main"
+cb_pnmixer="pnmixer"
+cb_pnmixer_deb="${cb_pnmixer}_0.5.1-crunchbang1_${deb_arch}.deb"
+
+# pnmixer - volume mixer for the system tray
+if [[ ! -e "${dpkg_info}/${cb_pnmixer}.list" ]]
+then
+    clear
+    wget -P $tmp_dir ${cb_archive}/${cb_pnmixer_deb}
+    dpkg -i ${tmp_dir}/$cb_pnmixer_deb
+fi
+}
+
+config_branch() {
+clear
+while :
+do
+cat << _EOF_
+Please select the default Debian branch for packages:
+
+0) ${deb_stable}/stable
+1) ${deb_testing}/testing
+2) ${deb_unstable}/unstable
+
+_EOF_
+
+read -n 1 -p "Your choice? [0-2] > "
+
+case $REPLY in
+    0)
+        printf "\nOK. System will be configured to track the stable branch...\n"
+        sleep 2
+        deb_branch="$deb_stable"
+        apt_preferences_stable
+        apt_sources_stable
+        break
+        ;;
+    1)
+        printf "\nOK. System will be configured to track the testing branch...\n"
+        sleep 2
+        deb_branch="$deb_testing"
+        apt_preferences_testing
+        apt_sources_testing
+        break
+        ;;
+    2)
+        printf "\nOK. System will be configured to track the unstable branch...\n"
+        sleep 2
+        deb_branch="$deb_unstable"
+        apt_preferences_unstable
+        apt_sources_unstable
+        break
+        ;;
+    *)
+        invalid_reply
+        ;;
+esac
+done
+}
+
+config_alternative() {
+clear
+update-alternatives --config editor
+clear
+update-alternatives --config x-terminal-emulator
+}
+
+config_desktop() {
+clear
+while :
+do
+cat << _EOF_
+Please select console-only or X environment:
+
+0) Do not install an X environment
+1) Openbox - lightweight window manager
+
+_EOF_
+
+read -n 1 -p "Your choice? [0-1] > "
+
+case $REPLY in
+    0)
+        printf "\nOK. No X environment will be installed...\n"
+        sleep 2
+        package_console
+        break
+        ;;
+    1)
+        printf "\nOK. Installing Openbox...\n"
+        sleep 2
+        package_console
+        package_xorg
+        package_openbox
+        package_theme
+        package_backport
+        package_extra
+        config_alternative
+        break
+        ;;
+    *)
+        invalid_reply
+        ;;
+esac
+done
+}
+
+config_blacklist_pcspkr() {
+local pcspkr_conf
+pcspkr_conf="/etc/modprobe.d/pcspkr-blacklist.conf"
+
+if [[ -e $pcspkr_conf ]]
+then
+    cp $pcspkr_conf ${pcspkr_conf}.$(date +%Y%m%dT%H%M%S).bak
+fi
+cat >> $pcspkr_conf << _EOF_
+# shutdown that annoying beep
+# note: always run 'update-initramfs -u -k all' after modifying modules
+blacklist pcspkr
+_EOF_
+}
+
+config_blacklist() {
+clear
+config_blacklist_pcspkr
+update-initramfs -u -k all
+}
+
+config_group() {
+set +e
+local user_groups
+user_groups="adm audio cdrom dialout dip floppy fuse netdev plugdev sudo \
+vboxusers video users"
+
+clear
+read -p "What will be your (non-root) user name? > " user_name
+printf "\nHello ${user_name}!\n"
+sleep 2
+if [[ ! -d /home/${user_name} ]]
+then
+    adduser $user_name
+fi
+for i in ${user_groups[@]}
+do
+    adduser $user_name $i
+done
+set -e
+}
+
+config_locale() {
+clear
+dpkg-reconfigure locales
+}
+
+config_timezone() {
+clear
+dpkg-reconfigure tzdata
+}
+
+au_revoir() {
+clear
+printf "$( penguinista ) .: All done!\n"
+}
+
+#: START
+run_options "$@"
+long_description
+confirm_start
+test_conditions
+
+#: APT
+config_branch
+apt_keys
+apt_package_list
+
+#: CONFIGURE
+config_desktop
+config_blacklist
+config_group
+config_locale
+config_timezone
+
+#: FINISH
+apt_package_purge
+au_revoir
