f336e9847e0f28b6edd7c86a66073938d0d38d9f
Daniel Wayne Armstrong <daniel@circuidipity.com>
2017-08-15 17:35:22 -0400

    first commit

---
+++ scripts/debian-setup/Library.sh
@@ -0,0 +1,262 @@
+#!/bin/bash
+# NAME="Library.sh"
+# BLURB="A library of functions for bash shell scripts"
+# GITHUB="https://github.com/vonbrownie"
+# SOURCE="$GITHUB/linux-post-install/tree/master/scripts/debian-stable-setup"
+set -eu
+
+# Place in local directory and call its functions by adding to script ...
+#   . ./Library.sh
+
+OPT_HELP="Run with '-h' for details."
+# ANSI escape codes
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+NC="\033[0m" # no colour
+
+
+L_echo_red() {
+echo -e "${RED}$1${NC}"
+}
+
+
+L_echo_green() {
+echo -e "${GREEN}$1${NC}"
+}
+
+
+L_echo_yellow() {
+echo -e "${YELLOW}$1${NC}"
+}
+
+
+L_banner_begin() {
+L_echo_yellow "\n--------[  $1  ]--------\n"
+}
+
+
+L_banner_end() {
+L_echo_green "\n--------[  $1 END  ]--------\n"
+}
+
+
+L_sig_ok() {
+L_echo_green "--> [ OK ]"
+}
+
+
+L_sig_fail() {
+L_echo_red "--> [ FAIL ]"
+}
+
+
+L_invalid_reply() {
+L_echo_red "\n'${REPLY}' is invalid input..."
+}
+
+
+L_invalid_reply_yn() {
+L_echo_red "\n'${REPLY}' is invalid input. Please select 'Y(es)' or 'N(o)'..."
+}
+
+
+L_penguin() {
+cat << _EOF_
+(O<
+(/)_
+_EOF_
+}
+
+
+L_run_script() {
+while :
+do
+    read -n 1 -p "Run script now? [yN] > "
+    if [[ $REPLY == [yY] ]]; then
+        echo -e "\nLet's roll then ..."
+        sleep 2
+        if [[ -x "/usr/games/sl" ]]; then
+            /usr/games/sl
+        fi
+        break
+    elif [[ $REPLY == [nN] || $REPLY == "" ]]; then
+        echo -e "\n$( L_penguin )"
+        exit
+    else
+        L_invalid_reply_yn
+    fi
+done
+}
+
+
+L_test_announce() {
+    echo -e "\n$( L_penguin ) .: Let's run a few tests before we begin ..."
+}
+
+
+L_test_required_file() {
+local FILE=$1
+local ERR="ERROR: file '$FILE' required but not found."
+if [[ ! -f "$FILE" ]]; then
+    L_echo_red "\n$( L_penguin ) .: $ERR"
+    exit 1
+fi
+}
+
+
+L_test_root() {
+local ERR="ERROR: script must be run with root privileges. $OPT_HELP"
+if (( EUID != 0 )); then
+    L_echo_red "\n$( L_penguin ) .: $ERR"
+    exit 1
+fi
+}
+
+
+L_test_homedir() {
+# $1 is $USER
+local ERR="ERROR: no USERNAME provided. $OPT_HELP"
+if [[ "$#" -eq 0 ]]; then
+    L_echo_red "\n$( L_penguin ) .: $ERR"
+    exit 1
+elif [[ ! -d "/home/$1" ]]; then
+    local ERR1="ERROR: a home directory for '$1' not found. $OPT_HELP"
+    L_echo_red "\n$( L_penguin ) .: $ERR1"
+    exit 1
+fi
+}
+
+
+L_test_internet() {
+local ERR="ERROR: script requires internet access to do its job. $OPT_HELP"
+local UP
+export UP
+UP=$( nc -z 8.8.8.8 53; echo $? ) # Google DNS is listening?
+if [[ $UP -ne 0 ]]; then
+    L_echo_red "\n$( L_penguin ) .: $ERR"
+    exit 1
+fi
+}
+
+
+L_test_datetime() {
+clear
+L_banner_begin "Confirm date + timezone"
+local LINK="<https://wiki.archlinux.org/index.php/time>"
+if [[ -x "/usr/bin/timedatectl" ]]; then
+    timedatectl
+else
+    echo -e "Current date is $( date -I'minutes' )"
+fi
+while :
+do
+    read -n 1 -p "Modify? [yN] > "
+    if [[ $REPLY == [yY] ]]; then
+        echo -e "\n\n$( L_penguin ) .: Check out datetime in Arch Wiki $LINK" 
+        echo "plus 'dpkg-reconfigure tzdata' for setting default timezone."
+        exit
+    elif [[ $REPLY == [nN] || $REPLY == "" ]]; then
+        clear
+        break
+    else
+        L_invalid_reply_yn
+    fi
+done
+}
+
+
+L_test_systemd_fail() {
+clear
+L_banner_begin "List 'systemctl --failed' units"
+systemctl --failed
+while :
+do
+    read -n 1 -p "Continue script? [yN] > "
+    if [[ $REPLY == [yY] ]]; then
+        clear
+        break
+    elif [[ $REPLY == [nN] || $REPLY == "" ]]; then
+         echo ""
+         L_penguin
+         exit
+     else
+         L_invalid_reply_yn
+     fi
+ done
+}
+
+
+L_test_priority_err() {
+clear
+L_banner_begin "Identify high priority errors"
+journalctl -p 0..3 -xn
+while :
+do
+    read -n 1 -p "Continue script? [yN] > "
+    if [[ $REPLY == [yY] ]]; then
+        clear
+        break
+    elif [[ $REPLY == [nN] || $REPLY == "" ]]; then
+        echo ""
+        echo -e "\n$( L_penguin ) .: Run 'journalctl -p 0..3 -xn' for errors."
+        exit
+    else
+        L_invalid_reply_yn
+    fi
+done
+}
+
+
+L_bak_file() {
+for f in "$@"; do cp "$f" "$f.$(date +%FT%H%M%S).bak"; done
+}
+
+
+L_apt_update_upgrade() {
+echo "Update packages and upgrade $HOSTNAME ..."
+apt update && apt -y full-upgrade
+L_sig_ok
+}
+
+
+L_conf_apt_src_stable() {
+# $1 is debian RELEASE
+local FILE="/etc/apt/sources.list"
+local MIRROR="http://deb.debian.org/debian/"
+local MIRROR1="http://security.debian.org/debian-security"
+local COMP="main contrib non-free"
+L_bak_file $FILE
+echo "Configure sources.list for '$1' ..."
+cat << _EOL_ > $FILE
+# Base repository
+deb $MIRROR $1 $COMP
+deb-src $MIRROR $1 $COMP
+
+# Security updates
+deb $MIRROR1 $1/updates $COMP
+deb-src $MIRROR1 $1/updates $COMP
+
+# Stable updates
+deb $MIRROR $1-updates $COMP
+deb-src $MIRROR $1-updates $COMP
+
+# Stable backports
+deb $MIRROR $1-backports $COMP
+deb-src $MIRROR $1-backports $COMP
+_EOL_
+L_sig_ok
+L_apt_update_upgrade
+}
+
+
+L_all_done() {
+local AU_REVOIR="All done!"
+if [[ -x "/usr/games/cowsay" ]]; then
+    L_echo_green "$( /usr/games/cowsay $AU_REVOIR )"
+else
+    echo -e "$( L_penguin ) .: $AU_REVOIR"
+fi
+}
+
+
---
+++ scripts/debian-setup/README.rst
@@ -0,0 +1,79 @@
+===================
+debian-stable-setup
+===================
+
+.. image:: files/debian_9_banner.png
+    :alt: Debian 9 Stretch
+    :width: 800px
+    :height: 75px
+
+SYNOPSIS
+========
+
+.. code-block:: bash
+
+    debian-stable-setup.sh [ options ] USER
+
+OPTIONS
+=======
+
+.. code-block:: bash
+
+    -h              print details
+    -b              basic setup (console only)
+    -p PKG_LIST     install packages from PKG_LIST
+
+EXAMPLE
+=======
+
+Post-install setup of a machine running Debian _stable_ release  for (existing) USER 'foo' ...
+
+.. code-block:: bash
+
+    $ sudo ./debian-stable-setup.sh foo
+
+Install packages from 'pkg-list' ...
+
+.. code-block:: bash
+
+    $ sudo ./debian-stable-setup.sh -p pkg-list foo
+
+DESCRIPTION
+===========
+
+Script **debian-stable-setup.sh** is ideally run immediately following the first successful boot into your new Debian installation.
+
+Building on a `minimal install <http://www.circuidipity.com/minimal-debian.html>`_ the system will be configured to track Debian's _stable_ release. A choice of either ...
+
+1) a basic console setup; or
+2) a more complete setup which includes the `i3 tiling window manager <http://www.circuidipity.com/i3-tiling-window-manager.html>`_ plus a packages collection suitable for a workstation; or
+3) install the `same list of packages as PKG_LIST <http://www.circuidipity.com/debian-package-list.html>`_
+
+... will be installed.
+
+USE
+===
+
+**0.** Install program folder on target machine.
+
+**1.** Copy ``config.sample`` to ``config`` and (optional) enable settings. All settings are **disabled** by default.
+
+**2.** Run program!
+
+DEPENDS
+=======
+
+``bash``
+
+Happy hacking!
+
+Author
+======
+
+| Daniel Wayne Armstrong (aka) VonBrownie
+| http://www.circuidipity.com
+
+License
+=======
+
+GPLv2. See ``LICENSE`` for more details.
